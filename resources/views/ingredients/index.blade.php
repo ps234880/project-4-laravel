e4422bf9ba177ba40b9c38904514687537283c65
Tong Sheng <ps234880@edu.summacollege.nl>
2023-01-24 15:56:08 +0100

    changed read ingredient design

--- resources/views/ingredients/index.blade.php
+++ resources/views/ingredients/index.blade.php
@@ -15,7 +15,7 @@
     @endif
 
     {{-- Gap from top and max width --}}
-    <div class="max-w-4xl mx-auto mt-10">
+    <div class="max-w-7xl mx-auto">
 
         {{-- Left right gap --}}
         <div class="px-4 sm:px-6 lg:px-8">
@@ -24,43 +24,46 @@
             <table class="mt-8 ring-1 ring-black ring-opacity-20">
                 <thead class="bg-slate-600">
                     <tr>
-                        <th scope="col" class="px-4 py-5 text-center text-xs font-medium uppercase text-slate-50">
+                        <th scope="col" class="px-4 py-5 text-center text-sm font-medium uppercase text-slate-50">
                             Name
                         </th>
-                        <th scope="col" class="px-4 py-5 text-center text-xs font-medium uppercase text-slate-50">
+                        <th scope="col" class="px-4 py-5 text-center text-sm font-medium uppercase text-slate-50">
                             Price
                         </th>
                         <th scope="col" class="px-4 py-5">
 
                         </th>
                         <th scope="col" class="px-4 py-5">
-                            <a href="/ingredients/create" class="text-slate-50 font-medium uppercase">
+                            <a href="/ingredients/create"
+                                class="text-slate-50 hover:text-slate-300 text-sm font-medium uppercase">
                                 Add ingredient
                             </a>
                         </th>
                     </tr>
                 </thead>
 
-                {{-- Index pizza --}}
-                <tbody class="bg-stone-50">
+                {{-- Ingredients --}}
+                <tbody>
                     @foreach ($ingredients as $ingredient)
                         <tr>
-                            <td class="whitespace-nowrap py-3 pl-4 pr-3 text-sm font-medium text-gray-900">
+                            {{-- Name --}}
+                            <td class="py-3 pl-4 pr-3 text-sm text-gray-900">
                                 {{ $ingredient->name }}
                             </td>
 
-                            <td class="whitespace-nowrap py-3 pl-4 pr-3 text-sm font-medium text-gray-900">
-                                {{ $ingredient->price }}
+                            {{-- Price --}}
+                            <td class="py-3 pl-4 pr-3 text-sm text-gray-900">
+                                € {{ $ingredient->price }}
                             </td>
 
                             {{-- Edit --}}
-                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium">
+                            <td class="py-4 pl-4 pr-3 text-sm">
                                 <a href="/ingredients/{{ $ingredient->id }}/edit"
                                     class="text-indigo-600 hover:text-indigo-900">Edit</a>
                             </td>
 
                             {{-- Delete --}}
-                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium">
+                            <td class="py-4 pl-4 pr-3 text-sm font-medium">
                                 <form action="{{ route('ingredients.destroy', $ingredient->id) }}" method="POST">
                                     @csrf
                                     @method('DELETE')
