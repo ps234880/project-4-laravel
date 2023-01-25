acab91f201c0f4038790d3a1a001b4e604a33a70
Tong Sheng <ps234880@edu.summacollege.nl>
2023-01-25 12:16:44 +0100

    added unit

--- database/seeders/IngredientSeeder.php
+++ database/seeders/IngredientSeeder.php
@@ -18,22 +18,27 @@ class IngredientSeeder extends Seeder
         DB::table('ingredients')->insert([
             'name' => "Cheese",
             'price' => 0.20,
+            'unit_id' => 1,
         ]);
         DB::table('ingredients')->insert([
             'name' => "Basil",
             'price' => 0.25,
+            'unit_id' => 1,
         ]);
         DB::table('ingredients')->insert([
             'name' => "Tomato sauce ",
             'price' => 0.80,
+            'unit_id' => 1,
         ]);
         DB::table('ingredients')->insert([
             'name' => "Ham",
             'price' => 1.00,
+            'unit_id' => 1,
         ]);
         DB::table('ingredients')->insert([
             'name' => "Egg",
             'price' => 0.60,
+            'unit_id' => 1,
         ]);
     }
 }
