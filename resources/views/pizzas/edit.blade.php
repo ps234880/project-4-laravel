2082bc1c30f86c530b491fbb707b58c64bbd4d1e
Tong Sheng <ps234880@edu.summacollege.nl>
2023-01-31 14:35:21 +0100

    changed pizza edit design

--- resources/views/pizzas/edit.blade.php
+++ resources/views/pizzas/edit.blade.php
@@ -7,6 +7,52 @@
 
     <div class="max-w-4xl mx-auto mt-5">
         <div class="px-4 sm:px-6 lg:px-8">
+            <div class="flex gap-5">
+                {{-- Add ingredient to pizza --}}
+                <div class="mb-6">
+                    <label for="name" class="block mb-2 font-medium text-slate-600">Add ingredients to
+                        pizza</label>
+                    @foreach ($ingredients as $ingredient)
+                        <table class="text-sm">
+                            <tr>
+                                <td class="text-slate-600">
+                                    {{ $ingredient->name }}
+                                </td>
+                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium">
+                                    <form action="{{ route('ingredient_pizza.store') }}" method="POST">
+                                        @csrf
+                                        <button class="text-green-600 hover:text-green-900">Add</button>
+                                        <input type="hidden" name="ingredient_id" value="{{ $ingredient->id }}">
+                                        <input type="hidden" name="pizza_id" value="{{ $pizza->id }}">
+                                    </form>
+                                </td>
+                            </tr>
+                        </table>
+                    @endforeach
+                </div>
+                {{-- Remove ingredient from pizza --}}
+                <div class="mb-6">
+                    <label for="name" class="block mb-2 font-medium text-slate-600">Remove
+                        ingredients</label>
+                    @foreach ($pizza->ingredients as $ingredient)
+                        <table class="text-sm">
+                            <tr>
+                                <td class="text-slate-600">
+                                    {{ $ingredient->name }}
+                                </td>
+                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium">
+                                    <form action="{{ route('ingredient_pizza.destroy', $pizza->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="text-red-600 hover:text-red-900">Remove</button>
+                                        <input type="hidden" name="ingredient_id" value="{{ $ingredient->id }}">
+                                    </form>
+                                </td>
+                            </tr>
+                        </table>
+                    @endforeach
+                </div>
+            </div>
             {{-- Edit form --}}
             <form method="POST" action="{{ route('pizzas.update', $pizza->id) }}">
                 @method('PATCH')
@@ -32,53 +78,6 @@
                     </button>
                 </div>
             </form>
-
-            {{-- Add ingredient to pizza --}}
-            <div class="mb-6">
-                <label for="name" class="block mb-2 text-sm font-medium text-slate-600">Add ingredients to
-                    pizza</label>
-                @foreach ($ingredients as $ingredient)
-                    <table>
-                        <tr>
-                            <td class="text-slate-600">
-                                {{ $ingredient->name }}
-                            </td>
-                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium">
-                                <form action="{{ route('ingredient_pizza.store') }}" method="POST">
-                                    @csrf
-                                    <button class="text-green-600 hover:text-green-900">Add</button>
-                                    <input type="hidden" name="ingredient_id" value="{{ $ingredient->id }}">
-                                    <input type="hidden" name="pizza_id" value="{{ $pizza->id }}">
-                                </form>
-                            </td>
-                        </tr>
-                    </table>
-                @endforeach
-            </div>
-
-            {{-- Remove ingredient from pizza --}}
-            <div class="mb-6">
-                <label for="name" class="block mb-2 text-sm font-medium text-slate-600">Remove
-                    ingredients</label>
-
-                @foreach ($pizza->ingredients as $ingredient)
-                    <table>
-                        <tr>
-                            <td class="text-slate-600">
-                                {{ $ingredient->name }}
-                            </td>
-                            <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium">
-                                <form action="{{ route('ingredient_pizza.destroy', $pizza->id) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="text-red-600 hover:text-red-900">Remove</button>
-                                    <input type="hidden" name="ingredient_id" value="{{ $ingredient->id }}">
-                                </form>
-                            </td>
-                        </tr>
-                    </table>
-                @endforeach
-            </div>
         </div>
     </div>
 </x-app-layout>
