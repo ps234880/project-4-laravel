36d1609bb078c137a68ac61e9586723ffbcfc583
Tong Sheng <ps234880@edu.summacollege.nl>
2023-02-03 22:40:32 +0100

    wip auth user

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -8,45 +8,42 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-                @foreach ($users as $user)
-                    <table class="ring-1 ring-black ring-opacity-20 rounded-lg">
-                        <thead class="bg-slate-600">
-                            <tr class="max-w-2xl">
-                                <td class="px-4 py-5 text-left text-sm uppercase text-slate-50 rounded-t-lg">
-                                    {{ $user->name }}
-                                </td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($user->orders as $order)
-                                <tr>
-                                    <td class="px-4 py-5 text-sm text-slate-600 text-left">
-                                        Order status: {{ $order->orderstatus->name }}
-                                        <a href="{{ route('orders.edit', $order->id) }}"
-                                            class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                    </td>
-                                </tr>
-                            @endforeach
+                @foreach ($orders as $order)
+                    @foreach ($users as $user)
+                        @if ($order->user_id != Auth::user()->id)
+                            <table class="ring-1 ring-black ring-opacity-20 rounded-lg">
+                                <thead class="bg-slate-600">
+                                    <tr class="max-w-2xl">
+                                        <td class="px-4 py-5 text-left text-sm uppercase text-slate-50 rounded-t-lg">
+                                            {{ $user->name }}
+                                        </td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($user->orders as $order)
+                                        <tr>
 
-                            @foreach ($order->orderlines as $orderline)
-                                <tr>
-                                    <td class="px-4 py-5 text-sm text-slate-600 text-left">
-                                        Pizza: {{ $orderline->pizza->name }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="px-4 py-5 text-sm text-slate-600 text-left">
-                                        Size: {{ $orderline->size->name }}
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td class="px-4 py-5 text-sm text-slate-600 text-left">
-                                        Amount: {{ $orderline->amount }}
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                                            <td class="px-4 py-5 text-sm text-slate-600 text-left">
+                                                Order status: {{ $order->orderstatus->name }}
+                                                <a href="{{ route('orders.edit', $order->id) }}"
+                                                    class="text-indigo-600 hover:text-indigo-900">Edit</a>
+                                            </td>
+
+                                        </tr>
+                                    @endforeach
+                                    @foreach ($order->orderlines as $orderline)
+                                        <tr>
+                                            <td class="px-4 py-5 text-sm text-slate-600 text-left">
+                                                Pizza: {{ $orderline->pizza->name }}
+                                                Size: {{ $orderline->size->name }}
+                                                Amount: {{ $orderline->amount }}
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        @endforeach
+                    @endif
                 @endforeach
             </div>
         </div>
