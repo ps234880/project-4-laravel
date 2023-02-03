04a1eefafad24be96a63bc6ec4be1046817530aa
Tong Sheng <ps234880@edu.summacollege.nl>
2023-02-03 18:54:24 +0100

    removed user_id

--- database/migrations/2023_01_11_092031_create_order_line_table.php
+++ database/migrations/2023_01_11_092031_create_order_line_table.php
@@ -16,17 +16,13 @@ return new class extends Migration
         Schema::create('order_line', function (Blueprint $table) {
             $table->id('id');
 
-            // // order_id
+            // order_id
             $table->unsignedBigInteger('order_id')->index();
             $table->foreign('order_id')->references('id')->on('orders')->cascadeOnDelete()->cascadeOnUpdate();
 
             // pizza_id
             $table->unsignedBigInteger('pizza_id')->index();
             $table->foreign('pizza_id')->references('id')->on('pizzas')->cascadeOnDelete()->cascadeOnUpdate();
-
-            // user_id
-            $table->unsignedBigInteger('user_id')->index();
-            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
 
             $table->string('size');
             $table->unsignedBigInteger('amount');
