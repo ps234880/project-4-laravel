f36bef077d5d7d147f090a0dda0fde31158866b6
Tong Sheng <ps234880@edu.summacollege.nl>
2023-02-03 14:07:39 +0100

    added comment

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -10,6 +10,7 @@
             {{-- Pizzas --}}
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 @foreach ($pizzas as $pizza)
+                    {{-- ingredient sum price per pizza --}}
                     @php
                         $sum = 0;
                         foreach ($pizza->ingredients as $ingredient) {
