92540e91b56a85c83e6cb3236216fb8c706378be
WahanH <Wahan2500@hotmail.com>
2023-02-03 18:20:00 +0100

    added 2 unit tests

--- tests/Unit/CrudTest.php
+++ tests/Unit/CrudTest.php
@@ -5,23 +5,25 @@ namespace Tests\Unit;
 use App\Http\Controllers\IngredientController;
 use App\Http\Controllers\PizzaController;
 use App\Http\Controllers\UnitController;
-
+use App\Models\Ingredient;
+use App\Models\Pizza;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\Request;
 use Tests\TestCase; // make sure to add this
 
 class CrudTest extends TestCase
 {
-    use RefreshDatabase;
-    public function test_store_method_ingredient()
+    public function test_store_method_pizza()
     {
-        // maak een unit aan
-        $unitController = new PizzaController();
-        $unitController->store(new Request(['id' => 30], ['name' => 'kiwi']));
-
-        $this->assertDatabaseHas('pizzas', ['id' => 30], ['name' => 'kiwi']);
+        $request = new Request(['name' => 'kiwi']);
+        Pizza::create($request->only(['name']));
+        $this->assertDatabaseHas('pizzas', ['name' => 'kiwi']);
     }
 
-   
-
+    public function test_store_method_ingredient()
+    {
+        $request = new Request(['name' => 'egg', 'price' => 0.5, 'unit_id' => 1]);
+        Ingredient::create($request->only(['name', 'price', 'unit_id']));
+        $this->assertDatabaseHas('ingredients', ['name' => 'egg', 'price' => 0.5]);
+    }
 }
