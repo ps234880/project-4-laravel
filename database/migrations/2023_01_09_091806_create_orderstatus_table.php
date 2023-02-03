3a2ef9d31726f5d227e8a7a0e7e56a460a15b591
Tong Sheng <ps234880@edu.summacollege.nl>
2023-02-03 19:38:41 +0100

    changed column name

--- database/migrations/2023_01_09_091806_create_orderstatus_table.php
+++ database/migrations/2023_01_09_091806_create_orderstatus_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('orderstatus', function (Blueprint $table) {
             $table->id('id');
-            $table->string('status')->nullable(false);
+            $table->string('name')->nullable(false);
         });
     }
 
