70575c9ca7dc429b10b1990a1ae53bd5b84486a1
00SUS <erwin.yuen@live.nl>
2023-02-06 17:28:22 +0100

    nav_fix

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,16 +5,13 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('pizzas.index') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
                     @hasanyrole('employee|admin')
                         <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index', 'users.edit', 'users.create', 'users.destroy')">
                             {{ __('Users') }}
@@ -27,12 +24,12 @@
                             {{ __('Units') }}
                         </x-nav-link>
                     @endrole
-                    <x-nav-link :href="route('orders.index')" :active="request()->routeIs('orders.index', 'orders.edit', 'orders.create', 'orders.destroy')">
-                        {{ __('Orders') }}
-                    </x-nav-link>
-
                     <x-nav-link :href="route('pizzas.index')" :active="request()->routeIs('pizzas.index', 'pizzas.edit', 'pizzas.create', 'pizzas.destroy')">
                         {{ __('Pizzas') }}
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('orders.index')" :active="request()->routeIs('orders.index', 'orders.edit', 'orders.create', 'orders.destroy')">
+                        {{ __('Orders') }}
                     </x-nav-link>
 
                     <x-nav-link :href="route('checkouts.index')" :active="request()->routeIs('checkouts.index', 'checkouts.edit', 'checkouts.create', 'checkouts.destroy')">
@@ -97,11 +94,6 @@
 
         <!-- Responsive Navigation Menu -->
         <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
-            <div class="pt-2 pb-3 space-y-1">
-                <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                    {{ __('Dashboard') }}
-                </x-responsive-nav-link>
-            </div>
 
             <!-- Responsive Settings Options -->
             <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
@@ -127,12 +119,12 @@
                         {{ __('Units') }}
                     </x-responsive-nav-link>
                     @endhasanyrole
-                    <x-responsive-nav-link :href="route('orders.index')">
-                        {{ __('Orders') }}
-                    </x-responsive-nav-link>
-
                     <x-responsive-nav-link :href="route('pizzas.index')">
                         {{ __('Pizzas') }}
+                    </x-responsive-nav-link>
+
+                    <x-responsive-nav-link :href="route('orders.index')">
+                        {{ __('Orders') }}
                     </x-responsive-nav-link>
 
                     <x-nav-link :href="route('checkouts.index')">
